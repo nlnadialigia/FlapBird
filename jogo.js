dcaf1f6bd041fa9854ab7122eda4213e4171de1e
nadiasantos <nadiasantos@prefeitura.corp>
2020-07-31 12:28:43 -0300

    refatoring

--- jogo.js
+++ jogo.js
@@ -1,155 +1,153 @@
-console.log('[StarDev] Flappy Bird');
+console.log('[StarDev] Flappy Bird')
 
-const sprites = new Image();
-sprites.src = './assets/sprites.png';
 
-const canvas = document.querySelector('canvas');
-const contexto = canvas.getContext('2d');
+const sprites = new Image()
+sprites.src = './assets/sprites.png'
 
+const canvas = document.querySelector('canvas')
+const contexto = canvas.getContext('2d')
+
+
+// [Plano de Fundo]
 const planoDeFundo = {
-  sX: 390,
-  sY: 0,
-  w: 275,
-  h: 204,
+  spriteX: 390,
+  spriteY: 0,
+  largura: 275,
+  altura: 204,
   x: 0,
   y: canvas.height - 204,
   desenha() {
-    contexto.fillStyle = '#60b9f5'
-    contexto.fillRect(0, 0, canvas.width, canvas.height)
+    contexto.fillStyle = '#70c5ce'
+    contexto.fillRect(0,0, canvas.width, canvas.height)
+
     contexto.drawImage(
       sprites,
-      planoDeFundo.sX, planoDeFundo.sY, // Sprite X, Sprite Y
-      planoDeFundo.w, planoDeFundo.h, // Tamanho do recorte na sprite
+      planoDeFundo.spriteX, planoDeFundo.spriteY,
+      planoDeFundo.largura, planoDeFundo.altura,
       planoDeFundo.x, planoDeFundo.y,
-      planoDeFundo.w, planoDeFundo.h,
+      planoDeFundo.largura, planoDeFundo.altura,
     )
+
     contexto.drawImage(
       sprites,
-      planoDeFundo.sX, planoDeFundo.sY, // Sprite X, Sprite Y
-      planoDeFundo.w, planoDeFundo.h, // Tamanho do recorte na sprite
-      (planoDeFundo.x + planoDeFundo.w), planoDeFundo.y,
-      planoDeFundo.w, planoDeFundo.h,
+      planoDeFundo.spriteX, planoDeFundo.spriteY,
+      planoDeFundo.largura, planoDeFundo.altura,
+      (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+      planoDeFundo.largura, planoDeFundo.altura,
     )
-  }
+  },
 }
 
+// [Chao]
 const chao = {
-  sX: 0,
-  sY: 610,
-  w: 224,
-  h: 112,
+  spriteX: 0,
+  spriteY: 610,
+  largura: 224,
+  altura: 112,
   x: 0,
-  y: canvas.height - 112, // o canvas - a h do objeto
+  y: canvas.height - 112,
   desenha() {
     contexto.drawImage(
       sprites,
-      chao.sX, chao.sY, // Sprite X, Sprite Y
-      chao.w, chao.h, // Tamanho do recorte na sprite
+      chao.spriteX, chao.spriteY,
+      chao.largura, chao.altura,
       chao.x, chao.y,
-      chao.w, chao.h,
+      chao.largura, chao.altura,
     )
+
     contexto.drawImage(
       sprites,
-      chao.sX, chao.sY, // Sprite X, Sprite Y
-      chao.w, chao.h, // Tamanho do recorte na sprite
-      (chao.x + chao.w), chao.y,
-      chao.w, chao.h,
+      chao.spriteX, chao.spriteY,
+      chao.largura, chao.altura,
+      (chao.x + chao.largura), chao.y,
+      chao.largura, chao.altura,
     )
-  }
+  },
 }
 
 function fazColisao(flappyBird, chao) {
-  const flappyBirdY = flappyBird.y + flappyBird.h
-  const chaoY = chao.y
-
-  if (flappyBirdY >= chaoY) {
+  const flappyBirdY = flappyBird.y + flappyBird.altura
+  
+  if (flappyBirdY >= chao.y) {
     return true
   }
   return false
 }
 
-function criaFlappyBird() {
-  const flappyBird = {
-    sX: 0,
-    sY: 0,
-    w: 33,
-    h: 24,
-    x: 10,
-    y: 50,
-    pulo: 4.6,
-    pula() {
-      console.log('devo pular');
-      flappyBird.velocidade = -flappyBird.pulo
-    },
-    gravidade: 0.25,
-    velocidade: 0,
-    atualiza() {
-      if (fazColisao(flappyBird, chao)) {
-        console.log('fez colisão');
+const flappyBird = {
+  spriteX: 0,
+  spriteY: 0,
+  largura: 33,
+  altura: 24,
+  x: 10,
+  y: 50,
+  pulo: 4.6,
+  pula(){
+    console.log('DEVO PULAR');
+    flappyBird.velocidade = - flappyBird.pulo
+  },
+  gravidade: 0.25,
+  velocidade: 0,
+  atualiza() {
+    if (fazColisao(flappyBird, chao)) {
+      console.log('COLISÃO');
 
-        mudaParaTela(Telas.INICIO)
-
-        return
-      }
-
-      flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade
-      flappyBird.y = flappyBird.y + flappyBird.velocidade
-    },
-    desenha() {
-      contexto.drawImage(
-        sprites,
-        flappyBird.sX, flappyBird.sY, // Sprite X, Sprite Y
-        flappyBird.w, flappyBird.h, // Tamanho do recorte na sprite
-        flappyBird.x, flappyBird.y,
-        flappyBird.w, flappyBird.h,
-      )
+      mudaParaTela(Telas.INICIO)
+      return
     }
+   
+
+    flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade
+    flappyBird.y = flappyBird.y + flappyBird.velocidade
+  },
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
+      flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+      flappyBird.x, flappyBird.y,
+      flappyBird.largura, flappyBird.altura,
+    )
   }
-  return flappyBird
 }
 
-
+/// [mensagemGetReady]
 const mensagemGetReady = {
   sX: 134,
   sY: 0,
   w: 174,
   h: 152,
   x: (canvas.width / 2) - 174 / 2,
-  y: 50, // o canvas - a algura do objeto
+  y: 50,
   desenha() {
     contexto.drawImage(
       sprites,
-      mensagemGetReady.sX, mensagemGetReady.sY, // Sprite X, Sprite Y
-      mensagemGetReady.w, mensagemGetReady.h, // Tamanho do recorte na sprite
-      mensagemGetReady.x, mensagemGetReady.y,
+      mensagemGetReady.sX, mensagemGetReady.sY,
       mensagemGetReady.w, mensagemGetReady.h,
+      mensagemGetReady.x, mensagemGetReady.y,
+      mensagemGetReady.w, mensagemGetReady.h
     )
   }
 }
 
-//
-//Telas
-//
-
-let globais = {}
-let TelaAtiva = {}
+// 
+// [Telas]
+// 
+let telaAtiva = {}
 function mudaParaTela(novaTela) {
   telaAtiva = novaTela
 
-  if (TelaAtiva.inicializa) {
-    telaAtiva.inicializa()
+  if (telaAtiva.iniciliza) {
+    telaAtiva.iniciliza()
   }
 }
 
 const Telas = {
   INICIO: {
-    inicializa() {
-      globais.flappyBird = criaFlappyBird()
-    },
     desenha() {
       planoDeFundo.desenha()
       chao.desenha()
-      globais.flappyBird.desenha()
+      flappyBird.desenha()
       mensagemGetReady.desenha()
     },
     click() {
@@ -165,31 +163,30 @@ Telas.JOGO = {
   desenha() {
     planoDeFundo.desenha()
     chao.desenha()
-    globais.flappyBird.desenha()
+    flappyBird.desenha()
   },
-  click() {
-    globais.flappyBird.pula()
+  click(){
+    flappyBird.pula()
   },
   atualiza() {
-    globais.flappyBird.atualiza()
+    flappyBird.atualiza()
   }
 }
 
 function loop() {
-  telaAtiva.desenha();
-  telaAtiva.atualiza();
+
+  telaAtiva.desenha()
+  telaAtiva.atualiza()
 
   requestAnimationFrame(loop)
 }
 
-window.addEventListener('click', function () {
-  if (telaAtiva.click) {
+
+window.addEventListener('click', function() {
+  if(telaAtiva.click) {
     telaAtiva.click()
   }
 })
 
 mudaParaTela(Telas.INICIO)
 loop()
-
-//10:53 - aula 3
-// não encontrei o erro
